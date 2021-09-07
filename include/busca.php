ac3e0d42c3e6a6d4af3243ea06675836069ae0b7
Stefanyssilva377 <89547923+Stefanyssilva377@users.noreply.github.com>
2021-09-07 18:21:52 -0300

    Add files via upload

--- include/busca.php
+++ include/busca.php
@@ -1,1 +1,11 @@
-
+<?php
+	$fileBusca = array();
+	for($i = 0; $i < count($file); $i++){
+		if(strpos($file[$i]->title, $busca) !== false){
+			
+			array_push($fileBusca, $file[$i]);
+		}
+	}
+	
+	$file = $fileBusca;
+?>
